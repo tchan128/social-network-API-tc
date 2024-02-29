f30cece2dafea8be835d8911cb1112bd241c1ffb
Tiffany <tchan12899@gmail.com>
2024-02-29 12:59:45 -0500

    Added User model and Schema

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true, 
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // email validator/matchers
+        },
+        thoughts: [thoughtSchema],
+        friends: [friendSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    // Retrieve length of friend array
+    .get(function () {
+        return (this.friends).length
+    });
+
+const User = model('user', userSchema)
+
+module.exports = User;
